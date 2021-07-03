d965fa39ff02fc89d5bce5de0ea8abf100e5ae94
Chris Powell <hosting@ampedwebdesign.co.uk>
2021-07-03 19:13:07 +0100

    Adding Background tests

---
+++ tests/Unit/BackgroundTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace AmpedWeb\GlideInABox\Tests\Feature;
+
+
+
+
+use AmpedWeb\GlideUrl\Tests\TestCase;
+
+class BackgroundTest extends TestCase
+{
+
+    public function testBgIsFluent()
+    {
+        $this->assertSame($this->glideUrl, $this->glideUrl->bg('abc'));
+    }
+
+    public function testBgSetsCorrectValue()
+    {
+        $this->glideUrl->bg('def');
+        $this->assertEquals('DEF', $this->glideUrl->getParams()['bg']);
+    }
+
+    public function testBackgroundSetsCorrectValue()
+    {
+        $this->glideUrl->background('0123CDEF');
+        $this->assertEquals('0123CDEF', $this->glideUrl->getParams()['bg']);
+    }
+}
